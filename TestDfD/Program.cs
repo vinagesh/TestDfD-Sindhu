28e3e88b0bc20947269a2445e181a8ffe1d32bb7
Sindhu Nagesh <vinagesh@microsoft.com>
2023-05-03 14:18:33 -0700

    Adding code to test DfD

---
+++ TestDfD/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+var secret = "DefaultEndpointsProtocol=https;AccountName=scusonlinefuncsa;AccountKey=zUGzjIZRwSYU03HgPLphNBH/LBEa1WqEtFaxZasP2P36FlmqWrF+YeZXaI4zmyXjlRMFEgGbezmkH56zg2T99Q==;EndpointSuffix=core.windows.net";
+Console.WriteLine($"Hello {secret}");
+var result = GetDivision(1);
+
+int GetDivision(int denominator)
+{
+    try
+    {
+        return 1 / denominator;
+    }
+    catch
+    {
+        return 0;
+    }
+}

35e6d9cec40c0b5e9136765e326faf0743b0faf2
Sindhu Nagesh <vinagesh@microsoft.com>
2023-05-03 14:31:41 -0700

    updates

--- TestDfD/Program.cs
+++ TestDfD/Program.cs
@@ -2,10 +2,26 @@
 using System;
 
 var secret = "DefaultEndpointsProtocol=https;AccountName=scusonlinefuncsa;AccountKey=zUGzjIZRwSYU03HgPLphNBH/LBEa1WqEtFaxZasP2P36FlmqWrF+YeZXaI4zmyXjlRMFEgGbezmkH56zg2T99Q==;EndpointSuffix=core.windows.net";
+var secret1 = "DefaultEndpointsProtocol=https;AccountName=scusonlinefuncsa;AccountKey=zUGzjIZRwSYU03HgPLphNBH/LBEa1WqEtFaxZasP2P36FlmqWrF+YeZXaI4zmyXjlRMFEgGbezmkH56zg2T99Q==;EndpointSuffix=core.windows.net";
+var secret2 = "DefaultEndpointsProtocol=https;AccountName=testlinefuncsa;AccountKey=zUUUIZRwSYU03HgPLphNBH/LBEa1WqEtFaxZasP2P36FlmqWrF+YeZXaI4zmyXjlRMFEgGbezmkH56zg2T99Q==;EndpointSuffix=core.windows.net";
 Console.WriteLine($"Hello {secret}");
 var result = GetDivision(1);
+var result = GetDivision2(1);
 
 int GetDivision(int denominator)
+{
+    try
+    {
+        return 1 / denominator;
+    }
+    catch
+    {
+        return 0;
+    }
+}
+
+
+int GetDivision2(int denominator)
 {
     try
     {

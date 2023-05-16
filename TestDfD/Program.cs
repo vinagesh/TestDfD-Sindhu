d6939f667022b818261f55b9f207d7148f3978b4
Sindhu Nagesh <vinagesh@microsoft.com>
2023-05-16 11:20:49 -0700

    chge

--- TestDfD/Program.cs
+++ TestDfD/Program.cs
@@ -2,6 +2,8 @@
 using System;
 
 var secret = "DefaultEndpointsProtocol=https;AccountName=scusonlinefuncsa;AccountKey=zUGzjIZRwSYU03HgPLphNBH/LBEa1WqEtFaxZasP2P36FlmqWrF+YeZXaI4zmyXjlRMFEgGbezmkH56zg2T99Q==;EndpointSuffix=core.windows.net";
+var secret1 = "DefaultEndpointsProtocol=https;AccountName=scusonlinefuncsa;AccountKey=zUGzjIZRwSYU03HgPLphNBH/LBEa1WqEtFaxZasP2P36FlmqWrF+YeZXaI4zmyXjlRMFEgGbezmkH56zg2T99Q==;EndpointSuffix=core.windows.net";
+var secret2 = "DefaultEndpointsProtocol=https;AccountName=testlinefuncsa;AccountKey=zUUUIZRwSYU03HgPLphNBH/LBEa1WqEtFaxZasP2P36FlmqWrF+YeZXaI4zmyXjlRMFEgGbezmkH56zg2T99Q==;EndpointSuffix=core.windows.net";
 Console.WriteLine($"Hello {secret}");
 var result = GetDivision(1);
 
